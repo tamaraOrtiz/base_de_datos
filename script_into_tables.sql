df35bb531ddee75230e7daa77b59172d4fd5fbb3
Adrian Aguero <iceberg.04@gmail.com>
2013-04-28 16:49:09 -0400

    Agregue el diseño logico de la base de datos ProyectoDB. Y tres script_ de creaciòn para mySql

---
+++ script_into_tables.sql
@@ -0,0 +1,5 @@
+USE grupo_1;
+INSERT INTO categorias (id,nombre,linea_credito)
+	VALUES	(1,'minorista',0);
+INSERT INTO cliente (id,nombre,direccion,telefono,contacto,mail,categoria_cliente)
+    VALUES (1,'Angel','Kaaguyrory','0975123456','Angel Miranda','angel@gmail.com',1);

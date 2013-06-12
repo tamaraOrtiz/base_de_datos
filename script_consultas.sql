fd5428a8ce68c194424e956eaa87bdfc7a6b0596
tamaraOrtiz <tamara.tfs>
2013-06-11 21:59:38 -0400

    actual

--- script_consultas.sql
+++ script_consultas.sql
@@ -1,9 +1,11 @@
 SELECT t1.producto_id, t2.nombre, t1.cantidad FROM Venta_detalles t1, Productos t2 
 	WHERE t1.producto_id = t2.id ORDER BY t1.cantidad DESC LIMIT 5;
 
-SELECT t1.cliente, t2.nombre, SUM(t3.monto_total) AS monto_total FROM Venta_Facturas t1, Clientes t2, Venta_detalles t3
+SELECT t1.cliente, t2.nombre, SUM(t3.monto_total) AS monto_total FROM Venta_facturas t1, Clientes t2, Venta_detalles t3
 	WHERE (t1.cliente = t2.id AND t3.venta_id = t1.id) GROUP BY t1.cliente ORDER BY t3.monto_total DESC LIMIT 5;
 
+SELECT t1.proveedor, t2.nombre, SUM(t1.monto_total) AS monto_total FROM Compra_facturas t1, Proveedores t2
+	WHERE (t1.proveedor = t2.id) GROUP BY t1.proveedor ORDER BY t1.monto_total DESC LIMIT 5;
 
 
 

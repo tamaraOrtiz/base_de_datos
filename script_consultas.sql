f2ef3a344815d61d4de42ef74b74a248d6243f6a
tamaraOrtiz <tamara.tfs>
2013-06-14 09:59:36 -0400

    actual

--- script_consultas.sql
+++ script_consultas.sql
@@ -1,8 +1,19 @@
 SELECT fecha_emision AS Fecha, deposito_egreso AS Deposito, COUNT(deposito_egreso) AS Total_de_Ventas FROM Venta_facturas 
-	WHERE (fecha_emision > "13-05-14" AND fecha_emision < "13-06-14") GROUP BY fecha_emision;
+	WHERE (fecha_emision BETWEEN "13-05-14" AND "13-06-14") GROUP BY fecha_emision;
 
 SELECT fecha_emision AS Fecha, deposito_ingreso AS Deposito, COUNT(deposito_ingreso) AS Total_de_Compras FROM Compra_facturas 
-	WHERE (fecha_emision > "13-05-14" AND fecha_emision < "13-06-14") GROUP BY fecha_emision;
+	WHERE (fecha_emision BETWEEN "13-05-14" AND "13-06-14") GROUP BY fecha_emision;
+
+SELECT * FROM Compra_facturas;
+SELECT * FROM Compra_detalles;
+SELECT * FROM Productos;
+SELECT * FROM Proveedores;
+SELECT t1.proveedor, t2.nombre, t3.producto_id, t4.nombre, MAX(t1.fecha_emision) AS fecha, t3.costo_unitario 
+	FROM Compra_facturas t1, Proveedores t2, Compra_detalles t3, Productos t4
+		WHERE (((t1.fecha_emision BETWEEN "13-05-15" AND "13-06-15") AND (t3.producto_id BETWEEN 2 AND 5)) AND((t1.proveedor = t2.id AND t3.producto_id = t4.id) AND t3.compra_id = t1.id))
+			GROUP BY t1.id,t3.costo_unitario,t4.id;
+
+
 
 SELECT t1.id, t1.fecha_vencimiento, t1.cliente, t2.nombre, t1.saldo FROM Venta_facturas t1, Clientes t2
 	WHERE ((t1.fecha_vencimiento < CURDATE() AND t1.saldo > 0) AND t1.cliente = t2.id);
@@ -16,8 +27,8 @@ SELECT t1.id, t1.fecha_vencimiento, t1.proveedor, t2.nombre, t1.saldo FROM Compr
 SELECT t1.id, t1.fecha_vencimiento, t1.proveedor, t2.nombre, t1.saldo FROM Compra_facturas t1, Proveedores t2
 	WHERE ((t1.fecha_vencimiento > CURDATE() AND t1.saldo > 0) AND (t1.proveedor = t2.id));
 
-SELECT t1.producto_id, t2.nombre, t1.cantidad FROM Venta_detalles t1, Productos t2 
-	WHERE t1.producto_id = t2.id ORDER BY t1.cantidad DESC LIMIT 5;
+SELECT t1.producto_id, t2.nombre, SUM(t1.cantidad) AS cantidad_vendidos FROM Venta_detalles t1, Productos t2 
+	WHERE t1.producto_id = t2.id GROUP BY t1.producto_id ORDER BY SUM(t1.cantidad) DESC LIMIT 5;
 
 SELECT t1.cliente, t2.nombre, SUM(t1.monto_total) AS monto_total FROM Venta_facturas t1, Clientes t2
 	WHERE (t1.cliente = t2.id ) GROUP BY t1.cliente ORDER BY t1.monto_total DESC LIMIT 5;
@@ -26,7 +37,7 @@ SELECT t1.proveedor, t2.nombre, SUM(t1.monto_total) AS monto_total FROM Compra_f
 	WHERE (t1.proveedor = t2.id) GROUP BY t1.proveedor ORDER BY t1.monto_total DESC LIMIT 5;
 
 SELECT t1.id AS Producto, t1.nombre AS Descripcion, t2.precio, MAX(t3.fecha_emision) AS Ultima_venta FROM Productos t1, Lista_precios t2,  Venta_detalles t4, Venta_Facturas t3
-	WHERE ((t3.fecha_emision < "13-05-14" OR t3.fecha_emision > "13-06-14") AND (t4.venta_id = t3.id AND t1.id = t4.producto_id AND t2.producto_id = t1.id)) GROUP BY t1.id;
+	WHERE ((t3.fecha_emision NOT BETWEEN "13-05-14" AND "13-06-14") AND (t4.venta_id = t3.id AND t1.id = t4.producto_id AND t2.producto_id = t1.id)) GROUP BY t1.id;
 
 
 
